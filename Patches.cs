87f5b2eec0c7fdcb5c335cf09dfdb165b074bfe5
BDC <BDC@IMYSTMAN12>
2024-12-14 21:33:40 +0800

    添加项目文件。

---
+++ Patches.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+namespace BALDI_FULL_INTERFACE.Patches
+{
+    [HarmonyPatch]
+    public class Patch
+    {
+        [HarmonyPatch(typeof(OptionsMenu), "Awake"), HarmonyPrefix]
+        public static bool Prefix(OptionsMenu __instance)
+        {
+            OptionsManager.Initialize(__instance);
+            return true;
+        }
+    }
+}
