366a34832daac18d7b4776d821e4a21c0040478f
IMystman12 <laihanzhi123456@163.com>
2026-08-24 08:32:07 +0800

    #

--- obj/Debug/netstandard2.0/BB+ Application Interface.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/BB+ Application Interface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BB+ Application Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a8a762b587887e7e7d6539b0d4693371c17c47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c85ad63622f636016e71028418f55c288e8f9c5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BB+ Application Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BB+ Application Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
